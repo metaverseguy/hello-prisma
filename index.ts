532bb7d5e30a24da4fbcc9969fbfa15384795618
AcePatterson <acepttrsn@gmail.com>
2024-03-07 21:52:24 +0900

    feat: querying database

--- index.ts
+++ index.ts
@@ -3,19 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  try {
-    await prisma.$connect();
-    console.log("Database connection successful!");
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-  }
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
+  await prisma.post.update({
+    where: {
+      slug: "my-first-post",
+    },
+    data: {
+      comments: {
+        createMany: {
+          data: [
+            { comment: "Great post!" },
+            { comment: "Can't wait to read more!" },
+          ],
+        },
+      },
     },
   });
-  console.dir(allUsers, { depth: null });
+  const posts = await prisma.post.findMany({
+    include: {
+      comments: true,
+    },
+  });
+
+  console.dir(posts, { depth: Infinity });
 }
 main()
   .catch(async (e) => {
